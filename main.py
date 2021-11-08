7d5d75cf6cfe55e69284c7e5f0fdb742fcf41ee4
Fraxmaro <frederickchosson@gmail.com>
2021-11-08 00:04:14 -0500

    First commit.

---
+++ main.py
@@ -0,0 +1,30 @@
+# Programme en language Python d un jeu de shooter
+#Télécharger le dossier assets : http://graven.yt/pygame-assets
+#Documentation pygame : https://www.pygame.org/wiki/GettingSt...
+#Site flaticon : https://www.flaticon.com/
+#Site bruitages sonores : http://www.universal-soundbank.com/
+
+# Press Maj+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+# On a besoin de ce module pour les jeux
+import pygame
+# on initialise le module
+pygame.init()
+# On nomme la fenetre du jeux
+pygame.display.set_caption("Shooter de Mathieu")
+# on ouvre la fenetre du jeux
+pygame.display.set_mode((500,300))
+
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
